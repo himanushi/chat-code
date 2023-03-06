f0d651d0c162c93bd632f31752459c8ceb348ac5
himanushi <himanushi@users.noreply.github.com>
2023-03-07 00:09:05 +0900

    fix

--- src/routes/chat/[id]/+page.svelte
+++ src/routes/chat/[id]/+page.svelte
@@ -7,13 +7,15 @@
 	import UserChat from './user-chat.svelte';
 	import type { PageData } from './$types';
 	import { interpret } from 'xstate';
+	import { apiKey } from '~/store/apiKey';
+	import { goto } from '$app/navigation';
 
 	export let data: PageData;
 
 	const chatService = interpret(chatMachine);
 
 	$: id = data.id;
-	$: if (id) {
+	$: if (id && $apiKey) {
 		chatService;
 	}
 
@@ -32,29 +34,38 @@
 	});
 </script>
 
-<ion-content bind:this={contentEle}>
-	<ion-list>
-		{#each $chatService?.context.messages as message}
-			{#if message.role === 'assistant'}
-				<RobotChat {message} />
-			{:else if message.role === 'user'}
-				<UserChat {message} />
-			{/if}
-		{/each}
-	</ion-list>
-</ion-content>
-<ion-footer>
-	<ion-toolbar>
-		<ion-buttons slot="start">
-			<ion-button>
-				<Icon name="person" fill color="blue" />
-			</ion-button>
-		</ion-buttons>
-		<ion-textarea rows={1} auto-grow={true} placeholder="Type a message" />
-		<ion-buttons slot="end">
-			<ion-button>
-				<Icon name="send" fill color="white" />
-			</ion-button>
-		</ion-buttons>
-	</ion-toolbar>
-</ion-footer>
+{#if $apiKey}
+	<ion-content bind:this={contentEle}>
+		<ion-list>
+			{#each $chatService?.context?.messages ?? [] as message}
+				{#if message.role === 'assistant'}
+					<RobotChat {message} />
+				{:else if message.role === 'user'}
+					<UserChat {message} />
+				{/if}
+			{/each}
+		</ion-list>
+	</ion-content>
+	<ion-footer>
+		<ion-toolbar>
+			<ion-buttons slot="start">
+				<ion-button>
+					<Icon name="person" fill color="blue" />
+				</ion-button>
+			</ion-buttons>
+			<ion-textarea rows={1} auto-grow={true} placeholder="Type a message" />
+			<ion-buttons slot="end">
+				<ion-button>
+					<Icon name="send" fill color="white" />
+				</ion-button>
+			</ion-buttons>
+		</ion-toolbar>
+	</ion-footer>
+{:else}
+	<ion-content>
+		<ion-item button on:click={() => goto('/me')}>
+			<Icon fill name="warning" color="yellow" start />
+			<ion-label> API key を設定してください</ion-label>
+		</ion-item>
+	</ion-content>
+{/if}
