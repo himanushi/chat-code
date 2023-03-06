a1b1562d17bf1aeef31bf6f02f3f3a60af040c42
himanushi <himanushi@users.noreply.github.com>
2023-03-06 19:55:38 +0900

    fix

--- src/machines/chat-machine.ts
+++ src/machines/chat-machine.ts
@@ -1,52 +1,64 @@
 import { Configuration, OpenAIApi } from 'openai';
-import { createMachine } from 'xstate';
+import { assign, createMachine } from 'xstate';
 
 // OpenAI APIの認証情報
 // const OPENAI_API_KEY = 'your-api-key-here';
 const OPENAI_MODEL = 'gpt-3.5-turbo';
-// const openai = new OpenAIApi(OPENAI_API_KEY, new Configuration({}));
+
+export const Models = ['gpt-3.5-turbo', 'gpt-3.5', 'gpt-3.4', 'gpt-3.3'];
 
 // XStateの状態マシン定義
-const chatMachine = createMachine({
-	schema: {
-		context: {} as { openai?: OpenAIApi },
-		events: {} as { type: 'INIT_OPENAI'; apiKey: string }
-	},
-	id: 'chat',
-	initial: 'idle',
-	context: {},
-	states: {
-		idle: {
-			on: {
-				INIT_OPENAI: {}
-			}
+const chatMachine = createMachine(
+	{
+		schema: {
+			context: {} as { openai?: OpenAIApi; model: string },
+			events: {} as { type: 'INIT_OPENAI'; apiKey: string }
 		},
-		loading: {
-			invoke: {
-				id: 'generate-chat',
-				src: async (_, { input }) => {
-					const response = await openai.createCompletion({
-						model: OPENAI_MODEL,
-						prompt: `User: ${input}\nAI:`,
-						max_tokens: 50,
-						n: 1,
-						stop: '\n'
-					});
-					const { choices } = response.data;
-					const { text } = choices[0];
-					return text?.trim();
-				},
-				onDone: {
-					target: 'success',
-					actions: (_, event) => console.log(event.data)
-				},
-				onError: {
-					target: 'failure',
-					actions: (_, event) => console.error(event.data)
+		id: 'chat',
+		initial: 'idle',
+		context: {},
+		states: {
+			idle: {
+				on: {
+					INIT_OPENAI: {
+						actions: 'initOpenAI'
+					}
 				}
-			}
-		},
-		success: {},
-		failure: {}
+			},
+			loading: {
+				invoke: {
+					id: 'generate-chat',
+					src: async (_, { input }) => {
+						const response = await openai.createCompletion({
+							model: OPENAI_MODEL,
+							prompt: `User: ${input}\nAI:`,
+							max_tokens: 50,
+							n: 1,
+							stop: '\n'
+						});
+						const { choices } = response.data;
+						const { text } = choices[0];
+						return text?.trim();
+					},
+					onDone: {
+						target: 'success',
+						actions: (_, event) => console.log(event.data)
+					},
+					onError: {
+						target: 'failure',
+						actions: (_, event) => console.error(event.data)
+					}
+				}
+			},
+			success: {},
+			failure: {}
+		}
+	},
+	{
+		actions: {
+			initOpenAI: assign({
+				openai: (_, { apiKey }) => new OpenAIApi(new Configuration({ apiKey }))
+			})
+		}
 	}
-});
+);
--- src/routes/me/+page.svelte
+++ src/routes/me/+page.svelte
@@ -5,6 +5,9 @@
 
 <ion-content>
 	<ion-list>
+		<ion-item-divider>
+			<ion-label> OpenAI Settings </ion-label>
+		</ion-item-divider>
 		<ion-item lines="none">
 			<Icon name="key" fill color="yellow" start />
 			<ion-label>API Key</ion-label>
