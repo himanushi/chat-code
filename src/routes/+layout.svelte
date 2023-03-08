db0de2966336a72c0ace59c1b32876f3937654bb
himanushi <himanushi@users.noreply.github.com>
2023-03-09 08:52:02 +0900

    loading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,12 +6,29 @@
 	import Menu from './menu.svelte';
 	import Icon from '~/components/icon.svelte';
 	import { highlightCode } from '~/store/highlightCode';
+	import { addMessages, getLocaleFromNavigator, init, isLoading, _ } from 'svelte-i18n';
+	import en from '~/i18n/en.json';
+	import ja from '~/i18n/ja.json';
+	import { locale } from '~/store/locale';
 
+	let ready = false;
 	onMount(async () => {
+		// initialize ionic
 		initialize({
 			animated: true,
 			mode: 'ios'
 		});
+
+		// initialize locale
+		addMessages('en', en);
+		addMessages('en-US', en);
+		addMessages('ja', ja);
+		await init({
+			fallbackLocale: 'en',
+			initialLocale: $locale ?? getLocaleFromNavigator()
+		});
+
+		ready = true;
 	});
 </script>
 
@@ -22,23 +39,31 @@
 	/>
 </svelte:head>
 
-<ion-app>
-	<ion-split-pane when="sm" content-id="main">
-		<Menu />
-		<div class="ion-page" id="main">
-			<ion-header id="menu">
-				<ion-toolbar>
-					<ion-buttons slot="start">
-						<ion-button>
-							<ion-menu-toggle>
-								<Icon name="menu" />
-							</ion-menu-toggle>
-						</ion-button>
-					</ion-buttons>
-					<ion-title> Chat Code </ion-title>
-				</ion-toolbar>
-			</ion-header>
-			<slot />
-		</div>
-	</ion-split-pane>
-</ion-app>
+{#if !$isLoading && ready}
+	<ion-app>
+		<ion-split-pane when="sm" content-id="main">
+			<Menu />
+			<div class="ion-page" id="main">
+				<ion-header id="menu">
+					<ion-toolbar>
+						<ion-buttons slot="start">
+							<ion-button>
+								<ion-menu-toggle>
+									<Icon name="menu" />
+								</ion-menu-toggle>
+							</ion-button>
+						</ion-buttons>
+						<!-- <ion-title> {$_('welcome')} </ion-title> -->
+					</ion-toolbar>
+				</ion-header>
+				<slot />
+			</div>
+		</ion-split-pane>
+	</ion-app>
+{:else}
+	<ion-app>
+		<ion-content>
+			<ion-spinner />
+		</ion-content>
+	</ion-app>
+{/if}
