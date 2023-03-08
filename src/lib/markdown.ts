50b3143778238598d1abb5f28dab9ad5ee684540
himanushi <himanushi@users.noreply.github.com>
2023-03-08 18:19:53 +0900

    fix

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,27 +1,18 @@
-import DOMPurify from 'dompurify';
-import { marked } from 'marked';
-import hljs from 'highlight.js';
-import 'highlight.js/styles/github.css';
+import { remark } from 'remark';
+import parse from 'remark-parse';
+import gfm from 'remark-gfm';
+import rehype from 'remark-rehype';
+import stringify from 'rehype-stringify';
+import highlight from 'rehype-highlight';
 
-const renderer = new marked.Renderer();
-renderer.link = (href, title, text) => {
-	if (href && (href.indexOf('http://') === 0 || href.indexOf('https://') === 0)) {
-		return `<a target="_blank" href="${href}" title="${title}">${text}</a>`;
-	}
-	return `<a href="${href}" title="${title}">${text}</a>`;
-};
+const marked = remark()
+	.use(parse)
+	.use(rehype)
+	.use(stringify)
+	.use(highlight, { detect: true, subset: ['javascript'], ignoreMissing: true })
+	.use(gfm);
 
-// renderer.code = (code: string, lang?: string, escaped?: boolean) => {
-// 	const languageClassName = lang ? `language-${lang}` : '';
-// 	const codeClassName = `code-block ${languageClassName}`;
-// 	const highlightedCode = escaped ? code : hljs.highlightAuto(code);
-// 	return `<pre class="${codeClassName}"><code class="${codeClassName}">${highlightedCode}</code></pre>`;
-// };
-
-// renderer.codespan = (text: string) => {
-// 	return `<code style="background-color: #eee; padding: 2px;">${text}</code>`;
-// };
-
-const config: DOMPurify.Config = { ADD_ATTR: ['target'] };
-
-export const markdown = (text: string) => DOMPurify.sanitize(marked(text, { renderer }), config);
+export const markdown = (
+	text: string,
+	callback: (err: Error | null | undefined, file: any | undefined) => void
+) => marked.process(text, callback);
