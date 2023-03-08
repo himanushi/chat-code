493259681ac0abd76a78e54914740c2cbabf97f7
himanushi <himanushi@users.noreply.github.com>
2023-03-09 06:57:03 +0900

    注意書き追加

--- src/routes/me/api-key.svelte
+++ src/routes/me/api-key.svelte
@@ -12,7 +12,7 @@
 <ion-item-divider>
 	<ion-label> OpenAI Settings </ion-label>
 </ion-item-divider>
-<ion-item lines="none">
+<ion-item>
 	<Icon name="key" fill color="yellow" start />
 	<ion-label>API Key</ion-label>
 	<ion-input
@@ -26,4 +26,15 @@
 			}
 		}}
 	/>
+</ion-item>
+<ion-item button target="_blank" href="https://platform.openai.com/account/api-keys">
+	<Icon name="link" color="yellow" start />
+	<ion-label>API Key の取得はこちらから</ion-label>
+</ion-item>
+<ion-item>
+	<Icon name="info" color="blue" start />
+	<ion-label class="ion-text-wrap">
+		API Key
+		はローカルに保存されています。これは静的なアプリで、サーバーでは保持していません。ブラウザのローカルストレージに保存されています。
+	</ion-label>
 </ion-item>
